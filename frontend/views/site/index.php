efe3ecd00553e04ad5ebf8dc5c41280fb397e1aa
hdari <hdari@HDFC>
2018-07-18 10:21:08 -0500

    Cambios en fronted/views/site/index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -48,6 +48,6 @@ $this->title = 'My Yii Application';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
         </div>
-
+        <h2> Prueba commit </h2>
     </div>
 </div>
