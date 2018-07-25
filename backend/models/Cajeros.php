4caa6542eef0d7da264cee680ec6cb90e91d9cdb
Luis Anrrango <luisitoylag@gmail.com>
2018-07-25 06:30:35 -0500

    modelo/Cajeros Vista

--- backend/models/Cajeros.php
+++ backend/models/Cajeros.php
@@ -57,7 +57,7 @@ class Cajeros extends \yii\db\ActiveRecord
             'CIUDADNACIMIENTO' => 'Ciudad Nacimiento',
             'DIRECCION' => 'Dirección',
             'TELEFONO' => 'Teléfono',
-            'EMAIL' => 'Emails',
+            'EMAIL' => 'Emailss',
             'ESTADO' => 'Estado',
         ];
     }
